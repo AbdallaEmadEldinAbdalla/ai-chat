7592e034d130a97e4febe4921f1b688ad97495fd
AbdallaEmadEldinAbdalla <116554738+AbdallaEmadEldinAbdalla@users.noreply.github.com>
2024-10-16 17:01:09 +0400

    fix

--- lib/drizzle/0000_keen_devos.sql
+++ lib/drizzle/0000_keen_devos.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS "Chat" (
 CREATE TABLE IF NOT EXISTS "User" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"email" varchar(64) NOT NULL,
+	"createdAt" timestamp NOT NULL,
 	"password" varchar(64)
 );
 --> statement-breakpoint
