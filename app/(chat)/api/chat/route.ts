ef55a88c8eb9cdf9f04e21fb1a419326316a31db
AbdallaEmadEldinAbdalla <116554738+AbdallaEmadEldinAbdalla@users.noreply.github.com>
2024-10-17 14:50:08 +0400

    enhancements.

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -39,7 +39,6 @@ export async function POST(request: Request) {
     return new Response("Unauthorized", { status: 401 });
   }
 
-
   const coreMessages = convertToCoreMessages(messages);
 
   const context = (await getContext(coreMessages[coreMessages.length - 1].content as string)).map(e => e.pageContent).join(', ')
@@ -47,7 +46,6 @@ export async function POST(request: Request) {
   const result = await streamText({
     model: customModel,
     system:
-      // "You are a highly knowledgeable real estate assistant for properties in France. Your role is to help users find properties, provide real estate market data, and assist with legal and financial questions related to purchasing, renting, or selling homes. You should respond in a professional, helpful, and polite manner, using conversational language. Answer questions in French unless specified otherwise. Always offer clear and concise information about properties, market trends, tax regulations, and local real estate procedures.",
       `You are a helpful and knowledgeable assistant that answers user queries strictly within the context: "${context}". Focus only on the information and guidelines specified by this context. Avoid unrelated content. Your responses should be concise, accurate, and directly relevant to the context provided. If the user asks for information beyond the specified context, politely guide them back to the context or clarify that you are restricted to discussing topics within the context. Maintain a professional tone and clear structure in your answers.`,
     messages: coreMessages,
     maxSteps: 5,
